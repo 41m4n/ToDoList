6a29c3de27d48d68a0d9f5bd0d38d8d58fba9ef4
Aiman <42263487+41m4n@users.noreply.github.com>
2025-10-26 12:34:06 +0800

    added ToDoList Component and model

---
+++ Models/ToDoList.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
